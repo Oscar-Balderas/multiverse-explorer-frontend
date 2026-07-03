49225b2dad27b8e3b7362d3ec56e73042d014bdd
Oscar Balderas <ob96@hotmail.es>
2026-07-03 14:37:24 -0600

    Add project structure and routes

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__copyright">© 2026 Multiverse Explorer</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+import Main from "../../components/Main/Main";
+
+function Home() {
+  return (
+    <section className="home">
+      <Main />
+    </section>
+  );
+}
+
+export default Home;
